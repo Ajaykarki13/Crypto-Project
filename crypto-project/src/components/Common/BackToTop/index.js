1aff9c6ff2003d70ded1ae4a1134b758f8dac158
Ajaykarki13 <ajaymum698@gmail.com>
2023-04-27 13:52:45 +0530

    day 15

--- crypto-project/src/components/Common/BackToTop/index.js
+++ crypto-project/src/components/Common/BackToTop/index.js
@@ -1,22 +1,23 @@
 import ArrowUpwardRoundedIcon from '@mui/icons-material/ArrowUpwardRounded';
-import React from 'react';
+import React,{useEffect} from 'react';
 import './styles.css'; 
 
 function BackToTop() {
 
+  useEffect(()=>{
     let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     mybutton.style.display = "flex";
    }else {
     mybutton.style.display = "none";
   }
 }
-
+},[])
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
